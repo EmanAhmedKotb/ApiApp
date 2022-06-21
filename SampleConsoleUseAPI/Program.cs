335d3a09d342f988391eb80bcd85884c3405759c
EmanAhmedKotb <89937428+EmanAhmedKotb@users.noreply.github.com>
2022-06-21 12:38:05 +0200

    Adding files

---
+++ SampleConsoleUseAPI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SampleConsoleUseAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new System.Net.Http.HttpClient();
+            var content = await client.GetStringAsync("https://localhost:44382/average?nums=5&nums=8&nums=7&nums=3&nums=2&nums=9");
+
+            Console.WriteLine(content);
+            Console.Read();
+        }
+    }
+}
